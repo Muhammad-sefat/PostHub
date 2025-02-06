b30a5605491e3dbd737c712eaf6411d2438576d6
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-02-06 21:44:22 +0600

    Final Commit

--- Backend/index.js
+++ Backend/index.js
@@ -7,14 +7,13 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middleware
+const corsOptions = {
+  origin: ["https://durantotravelbd.web.app", "http://localhost:5173"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
-app.use(
-  cors({
-    origin: "*",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
